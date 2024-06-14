e72e413ac8d24f12637f9331e8f109fc652de9f2
Brandon Gonzalez <brajgon@gmail.com>
2024-06-13 21:15:54 -0600

    fixed index.js

--- server/index.js
+++ server/index.js
@@ -1,22 +1,27 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 4000
-const { getAllPosts, getCurrentUserPosts, addPost, editPost, deletePost } = require('./controllers/posts');
-const { register, login } = require('./controllers/auth');
-const isAuthenticated = require('./middleware/isAuthenticated');
-
+const PORT = process.env.PORT || 4000;
+const {
+  getAllPosts,
+  getCurrentUserPosts,
+  addPost,
+  editPost,
+  deletePost,
+} = require("./controllers/posts");
+const { register, login } = require("./controllers/auth");
+const { isAuthenticated } = require("./middleware/isAuthenticated");
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/register', register);
-app.post('/login', login);
+app.post("/register", register);
+app.post("/login", login);
 
-app.get('/posts', getAllPosts);
-app.get('/userposts/:userId', getCurrentUserPosts);
-app.post('/posts', isAuthenticated, addPost);
-app.put('/posts/:id', isAuthenticated, editPost);
-app.delete('/posts/:id', isAuthenticated, deletePost);
+app.get("/posts", getAllPosts);
+app.get("/userposts/:userId", getCurrentUserPosts);
+app.post("/posts", isAuthenticated, addPost);
+app.put("/posts/:id", isAuthenticated, editPost);
+app.delete("/posts/:id", isAuthenticated, deletePost);
 
 app.listen(PORT, () => console.log(`Server Running on ${PORT}`));
